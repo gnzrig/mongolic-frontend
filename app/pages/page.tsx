34b03d4049a4dedcd9deecce34e705d7c9ba792b
= <=>
2024-10-18 23:52:36 +0800

    1st

---
+++ app/pages/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Menu from "../components/menu/menu";
+
+const Base = () => {
+  return (
+    <div>
+      <Menu />
+      <div>Menu</div>
+    </div>
+  );
+};
+
+export default Base;
